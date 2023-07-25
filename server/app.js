9d3256f4010082b451040a870dbfa410e824dc4a
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-07-25 13:32:11 -0700

    testing secure false for render

--- server/app.js
+++ server/app.js
@@ -102,7 +102,7 @@ const sendEmail = (recipientUserObj, partnerUserObj) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
