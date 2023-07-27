1cf3e2e969988e6bb9cc4ce14892b8ef9aba6f64
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-07-27 13:54:40 -0700

    pushing reports page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,12 +6,10 @@
   - handle empty hobbies and passions
   - confirm required fields & minimum length
 
-
-
 =====================================================*/
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import Reports from './pages/reports'
+import Home from './pages/Home'
+import Reports from './pages/Reports'
 
 
 
