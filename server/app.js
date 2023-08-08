4fd759401c4e996ec6414915930b08971b8ee08d
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-08-08 15:11:58 -0700

    testing internal smtp

--- server/app.js
+++ server/app.js
@@ -210,7 +210,10 @@ const sendEmail = (recipientUserObj, partnerUserObj) => {
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     secure: false,
-
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
   });  
   
 
