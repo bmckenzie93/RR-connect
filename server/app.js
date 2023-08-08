5414224f500969ad0e4da4a3d29c236e4cfff541
Brandon McKenzie <dr.bmckenzie93@gmail.com>
2023-08-08 12:29:01 -0700

    testing production smtp

--- server/app.js
+++ server/app.js
@@ -147,7 +147,11 @@ const sendEmail = (recipientUserObj, partnerUserObj) => {
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: false
+    secure: false,
+    auth: {
+      user: '',
+      pass: ''
+    }
   });  
   
 
